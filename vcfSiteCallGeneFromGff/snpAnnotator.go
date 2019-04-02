a89f11db0105a381e5efb1ce33572673cdf4b53e
tigernaxo <tigernaxo.co.cc@gmail.com>
2019-04-02 16:10:32 +0800

    refactor tmp

--- vcfSiteCallGeneFromGff/snpAnnotator.go
+++ vcfSiteCallGeneFromGff/snpAnnotator.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"log"
 	"os"
+	"regexp"
 	"sort"
+	"strconv"
+	"strings"
 )
 
 // VCFAnnotator 的除非是使用bed等等0base的系統，定位皆為1base。
@@ -37,4 +41,83 @@ func (v *VCFAnnotator) AnnotateAndSave(fileName string) {
 			}
 		}
 	}
+}
+
+type Feature struct {
+	Start  int    // gff col 4
+	End    int    // gff col 5
+	Name   string // gff col9 extract Name=name
+	Strand byte   // gff col 7
+}
+
+// FeatureSet 保存同一類特徵、列表
+type FeatureSet struct {
+	Class    string // gff col 3
+	Features []*Feature
+}
+
+// SiteFromVCF 抽取VCF裡面的位址，回傳一個int slice
+func SiteFromVCF(vcf string) []int {
+	fmt.Printf("Extracting site information from vcf file: [%s].\n", vcf)
+	f, err := os.Open(vcf)
+	if err != nil {
+		log.Panic(err)
+	}
+	defer f.Close()
+
+	sites := make([]int, 0, 10000)
+	scanner := bufio.NewScanner(f)
+	// lineArr := make([]string, 2)
+	var line string
+	for scanner.Scan() {
+		// do somethign with line (scanner.Text)
+		line = scanner.Text()
+		lineArr := strings.Split(line, "\t")
+		if !strings.HasPrefix(lineArr[0], "#") {
+			i, err := strconv.Atoi(lineArr[1])
+			sites = append(sites, i)
+			if err != nil {
+				log.Panic(err)
+			}
+		}
+	}
+
+	return sites
+}
+
+// FeatureSetFromGFF 從GFF3裡面抽取特徵的範圍(start, end)、正負股、名稱
+func FeatureSetFromGFF(featureClass, gffFile string) *FeatureSet {
+	fmt.Printf("Extracting feature class [%s] from file [%s]\n", featureClass, gffFile)
+	fs := FeatureSet{
+		Class:    featureClass,
+		Features: make([]*Feature, 0, 100000),
+	}
+	f, err := os.Open(gffFile)
+	if err != nil {
+		log.Panic(err)
+	}
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+	pattern := regexp.MustCompile(`Name=[^;$]*`)
+	for scanner.Scan() {
+		lineArr := strings.Split(scanner.Text(), "\t")
+		if !strings.HasPrefix(lineArr[0], "#") && len(lineArr) == 9 && lineArr[2] == featureClass {
+			start, err := strconv.Atoi(lineArr[3])
+			if err != nil {
+				log.Panic(err)
+			}
+			end, err := strconv.Atoi(lineArr[4])
+			if err != nil {
+				log.Panic(err)
+			}
+			fs.Features = append(fs.Features, &Feature{
+				Start:  start,
+				End:    end,
+				Name:   strings.TrimPrefix(string(pattern.Find([]byte(lineArr[8]))), "Name="),
+				Strand: byte(lineArr[6][0]),
+			})
+		}
+	}
+	return &fs
 }
