3637ced9389f6f83440ea931e69a4795e3e1e3ca
tigernaxo <tigernaxo.co.cc@gmail.com>
2019-04-03 15:14:07 +0800

    histogramPlotter prototype

--- histogramPlotter/main.go
+++ histogramPlotter/main.go
@@ -1,77 +1,72 @@
 package main
 
 import (
-	"fmt"
 	"image"
 	"image/color"
 	"image/jpeg"
 	"log"
+	"math"
 	"os"
+	"strconv"
+
+	"github.com/nfnt/resize"
+	"github.com/tigernaxo/ATRIwork/fileformat"
 )
 
-const (
-	dx = 500
-	dy = 200
-)
-
+// accept: width vcf-file maxLength
 func main() {
 
-	file, err := os.Create("test.jpg")
+	vcfFile := os.Args[1]
+	width, err := strconv.Atoi(os.Args[2])
+	if err != nil {
+		log.Panic(err)
+	}
+	minUnit := 1000000
+	genomeLength := 4827641
+	maxLength := int(math.Ceil(float64(genomeLength) / float64(minUnit)))
+	drawColor := color.NRGBA{uint8(255), 0, 0, 255}
+	bgColor := color.NRGBA{218, 236, 198, 255}
+	redundantColor := color.NRGBA{255, 255, 255, 255}
+
+	snps := fileformat.SiteFromVCF(vcfFile)
+	file, err := os.Create(vcfFile + ".jpg")
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer file.Close()
-	rgba := image.NewRGBA(image.Rect(0, 0, dx, dy))
-	for x := 0; x < dx; x++ {
-		for y := 0; y < dy; y++ {
-			rgba.Set(x, y, color.NRGBA{uint8(x % 256), uint8(y % 256), 0, 255})
-		}
+	rgba := image.NewRGBA(image.Rect(0, 0, maxLength, 1))
+	for x := 0; x < maxLength; x++ {
+		rgba.Set(x, 0, bgColor)
 	}
 
-	fmt.Println(rgba.At(400, 100))    //{144 100 0 255}
-	fmt.Println(rgba.Bounds())        //(0,0)-(500,200)
-	fmt.Println(rgba.Opaque())        //true，其完全透明
-	fmt.Println(rgba.PixOffset(1, 1)) //2004
-	fmt.Println(rgba.Stride)          //2000
-	jpeg.Encode(file, rgba, nil)      //将image信息存入文件中
+	for x := 0; x < len(snps); x++ {
+		rgba.Set(snps[x], 0, drawColor)
+		if snps[x]+width < maxLength && snps[x]-width > 0 {
+			for i := snps[x] - width; i <= snps[x]+width; i++ {
+				rgba.Set(i, 0, drawColor)
+			}
+		} else if snps[x]+width > maxLength && snps[x]-width > 0 {
+			for i := snps[x] - width; i <= maxLength; i++ {
+				rgba.Set(i, 0, drawColor)
+			}
+		} else if snps[x]-width < 0 && snps[x]+width < maxLength {
+			for i := 0; i < snps[x]+width; i++ {
+				rgba.Set(i, 0, drawColor)
+			}
+		}
+	}
+	if maxLength > genomeLength {
+		for i := genomeLength + 1; i <= maxLength; i++ {
+			rgba.Set(i, 0, redundantColor)
+		}
+
+	}
+	resizedImg := resize.Resize(1920, 20, rgba, resize.NearestNeighbor)
+
+	// fmt.Println(rgba.At(400, 100))     //{144 100 0 255}
+	// fmt.Println(rgba.Bounds())         //(0,0)-(500,200)
+	// fmt.Println(rgba.Opaque())         //true，其完全透明
+	// fmt.Println(rgba.PixOffset(1, 1))  //2004
+	// fmt.Println(rgba.Stride)           //2000
+	jpeg.Encode(file, resizedImg, nil) //将image信息存入文件中
 }
-
-// func main() {
-// 	// snps := fileformat.SiteFromVCF("./data/snp.vcf")
-// 	zr := image.Rectangle{
-// 		Min: image.Point{
-// 			X: 0,
-// 			Y: 0,
-// 		},
-// 		Max: image.Point{
-// 			X: 5,
-// 			Y: 1,
-// 		},
-// 	}
-// 	img := image.NewNRGBA(zr)
-// 	for x := 1; x < 5; x++ {
-// 		img.SetNRGBA(x, 1, color.NRGBA{uint8(255), 0, 0, 100})
-// 	}
-// 	resizedImg := resize.Resize(5, 1, img, resize.Lanczos3)
-// 	// fmt.Println(len(snps))
-
-// 	SavePng(resizedImg, "image.png")
-// }
-
-// // SavePng save image as png
-// func SavePng(img image.Image, name string) {
-// 	f, err := os.Create(name)
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-
-// 	if err := png.Encode(f, img); err != nil {
-// 		f.Close()
-// 		log.Fatal(err)
-// 	}
-
-// 	if err := f.Close(); err != nil {
-// 		log.Fatal(err)
-// 	}
-
-// }
