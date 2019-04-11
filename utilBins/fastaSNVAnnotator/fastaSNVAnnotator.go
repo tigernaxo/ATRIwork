d471390631f24ab60c1a5ed3ebca575d444226b7
tigernaxo <tigernaxo.co.cc@gmail.com>
2019-04-11 22:46:30 +0800

    modify fastaSNVAnnotator comment

--- utilBins/fastaSNVAnnotator/fastaSNVAnnotator.go
+++ utilBins/fastaSNVAnnotator/fastaSNVAnnotator.go
@@ -6,29 +6,15 @@ import (
 	"time"
 
 	"github.com/tigernaxo/ATRIwork/fileformat"
-	// github.com/tigernaxo/ATRIwork/histogram
 	"github.com/tigernaxo/ATRIwork/snv"
 )
 
 // Input:
-// 1. intensity
-// 2. ref gff
-// 3. ...seqs
-
-// Fixed:
-// 4.color
-// 5.bgcolor
-// 6.min Unit ( ex:1000000, to decide convasLength )
-// 7.out dimension
-// 8.out name
+// 1. ref gff
+// 2. ...seqs
 
 // Output
-// 1.histograms
-
-// local import need
-// github.com/tigernaxo/ATRIwork/fileformat
-// github.com/tigernaxo/ATRIwork/histogram
-// github.com/tigernaxo/ATRIwork/snv
+// 1.annotation tsv
 
 func main() {
 	gff := os.Args[1]
