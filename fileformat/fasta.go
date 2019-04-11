416d82f791ce3256de8c811cda3193ba56f23636
tigernaxo <tigernaxo.co.cc@gmail.com>
2019-04-11 13:32:20 +0800

    histogram plotter debugging

--- fileformat/fasta.go
+++ fileformat/fasta.go
@@ -14,27 +14,37 @@ func ReadSingleFasta(fasta string) (id string, sequence []byte) {
 	}
 
 	idByte := make([]byte, 0, 50)
-	seq := make([]byte, 0, 10000) // > 62, \n10
-	// - 45, A-Z 65-9, a-z 97-122
+	seq := make([]byte, 0, 10000)
+	// > 62, \n 10, - 45
+	// A-Z 65-90, a-z 97-122
 	isLineStart, isIDSec, isSeqSec := true, false, false
+	// 行末\n              isLineStart = true
+	// id section行末的\n  IDsec = false
+	// seq section行末\n   isLineStart = true
+	// 每行第一個byte是\n       isLineStart = true
+	// 每行第一個byte是'>'       isIDsec = true
+	// 每行第一個byte但不是>\n 如果是其他: 開始或繼續seq section
+	// 每行第一個byte但不是>\n 如果是>: 結束seq section開始id section
 
 	for _, b := range content {
-		// if i == 4857462 {
-		// 	log.Panicf("i reaches %d, char is %d\n", i, int(b))
-		// }
 		switch b {
 		case 10:
 			if isLineStart {
-				isLineStart, isIDSec, isSeqSec = true, false, false
+				// 空白行(^$)的情況
+				isLineStart = true
 			} else if isIDSec {
+				// 到達ID section尾端
 				isLineStart, isIDSec, isSeqSec = true, false, true
-			} else {
+			} else if isSeqSec {
+				// 在Seq section內換行
 				isLineStart, isIDSec = true, false
 			}
 		case 62:
 			if isLineStart {
+				// >在第一個char就啟動id section
 				isLineStart, isIDSec, isSeqSec = false, true, false
 			} else {
+				// >在其他位置就直接加上去
 				seq = append(seq, b)
 			}
 		default:
